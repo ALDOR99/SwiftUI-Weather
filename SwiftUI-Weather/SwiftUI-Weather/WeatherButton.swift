4c59a02626d26d06ba6ea69891f1d079a2659a67
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-01-14 02:09:06 +0300

    0.4

--- SwiftUI-Weather/SwiftUI-Weather/WeatherButton.swift
+++ SwiftUI-Weather/SwiftUI-Weather/WeatherButton.swift
@@ -4,27 +4,38 @@
 //
 //  Created by Ali Durna on 12.01.2024.
 //
+
 import SwiftUI
 
+// WeatherButton adında bir SwiftUI View yapısı oluşturulur.
 struct WeatherButton: View {
     
-    var title: String
-    var textColor : Color
-    var backgroundColor : Color
+    // WeatherButton yapısının özellikleri tanımlanır.
+    var title: String           // Düğme üzerinde görüntülenecek metin.
+    var textColor : Color       // Metin rengi.
+    var backgroundColor : Color // Düğme arkaplan rengi.
     
+    // View yapısının içeriğini tanımlayan body bloğu.
     var body: some View {
         
-        Text(title)
-            .frame(width: 280,height: 50)
-            .background(backgroundColor.gradient)
-            .foregroundColor(textColor)
-            .font(.system(size: 20,weight: .bold,design: .default))
-            .cornerRadius(10)
+        // VStack, dikey bir düzen oluşturmak için kullanılır.
+        VStack(spacing:20){
+            
+            // Text ile düğme üzerindeki metin eklenir.
+            Text(title)
+                .bold()                             // Metni kalın yapar.
+                .frame(width: 280, height: 50)     // Düğme boyutu belirlenir.
+                .background(backgroundColor)       // Düğmenin arkaplan rengi belirlenir.
+                .cornerRadius(10)                   // Düğmenin köşe yuvarlatma değeri belirlenir.
+        }
+        .foregroundColor(textColor)                // Düğme metin rengi belirlenir.
     }
 }
 
-struct WeatherButton_Previews:PreviewProvider{
-    static var previews: some View{
+// WeatherButton yapısının önizlemesini sağlayan bir PreviewProvider yapısı oluşturulur.
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        // Önizleme, örnek bir WeatherButton nesnesini içerir.
         WeatherButton(title: "Test Title", textColor: .white, backgroundColor: .blue)
     }
 }
